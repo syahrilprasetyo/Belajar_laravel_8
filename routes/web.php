f86a1432dffec80c5ef9e6cba7434814c08b3415
Syahril Prasetyo <syahrilpras2017@gmail.com>
2021-03-08 01:39:27 +0700

    database migration, structur folder, membaca data di table menit 4:10

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/products', function () {
+    return view('products.index');
 });
